e0d0ed057a831124d281e32dd8c25fba6d7aa3a1
zhenglongyu <31196983+zhenglongyu@users.noreply.github.com>
2018-04-28 23:08:53 +0800

    增加了Kinect视频采集包并修正了opencv版本的bug

---
+++ iai_kinect2/kinect2_capture/src/kinect2_capture_node.cpp
@@ -0,0 +1,128 @@
+//标准C++头文件
+#include <iostream>
+#include <string>
+
+//OpenCV头文件
+#include<opencv2/opencv.hpp>
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+
+//ROS头文件
+#include <ros/ros.h>
+#include <ros/spinner.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+#include <kinect2_bridge/kinect2_definitions.h>
+
+//kinect相机话题
+std::string image_rgb_str =  "/kinect2/qhd/image_color_rect";
+std::string image_depth_str = "/kinect2/qhd/image_depth_rect";
+std::string cam_info_str = "/kinect2/qhd/camera_info";
+
+//图像保存
+std::string video_save_path = "/home/zhenglongyu/data/kinect_recognition";
+std::string rgb_video_path = video_save_path + "/rgb.avi";
+std::string depth_video_path = video_save_path + "/depth.avi";
+cv::VideoWriter depth_video_writer, rgb_video_writer;
+
+void RecognitionCallback(
+        const sensor_msgs::ImageConstPtr image_rgb,
+        const sensor_msgs::ImageConstPtr  image_depth,
+        const sensor_msgs::CameraInfoConstPtr  cam_info
+        )
+{
+    //转换ROS图像消息到opencv图像
+    cv::Mat mat_image_rgb = cv_bridge::toCvShare(image_rgb)->image;
+    cv::Mat mat_image_depth = cv_bridge::toCvShare(image_depth)->image;
+
+    //调整图像大小以便显示
+    cv::Mat image_rgb_show;
+    cv::Mat image_depth_show;
+    image_rgb_show = mat_image_rgb.clone();
+    image_depth_show = mat_image_depth.clone();
+
+    ROS_INFO("cols is %d",mat_image_rgb.cols);
+    ROS_INFO("rows is %d",mat_image_rgb.rows);
+    //显示彩色图和深度图
+    try
+    {
+        //cv::imshow(window_rgb_top, cv_bridge::toCvShare(image_rgb)->image);
+        cv::imshow("rgb_video", image_rgb_show);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("Could not show rgb images!");
+    }
+    try
+    {
+        cv::imshow("depth_video", image_depth_show);
+    }
+    catch(cv_bridge::Exception& e)
+    {
+        ROS_ERROR("Could not show depth images!");
+    }
+
+//    ROS_INFO("write depth video");
+//    depth_video_writer<<image_depth_show;
+    ROS_INFO("writer rgb video");
+    rgb_video_writer<<image_rgb_show;
+
+}
+
+//void ReleaseRecognition( )
+//{
+////    cv::destroyAllWindows();
+//}
+
+
+int main(int argc, char ** argv)
+{
+    //启动ROS节点并获取句柄
+    ros::init(argc, argv, "kinect2_caption");
+    ros::NodeHandle nh;
+
+    //订阅话题
+    message_filters::Subscriber<sensor_msgs::Image> image_rgb_sub(nh, image_rgb_str, 1);
+    message_filters::Subscriber<sensor_msgs::Image>image_depth_sub(nh, image_depth_str, 1);
+    message_filters::Subscriber<sensor_msgs::CameraInfo>cam_info_sub(nh,  cam_info_str, 1);
+
+//    //发布话题
+//    image_transport::ImageTransport it(nh);
+//    image_rgb_pub = it.advertise(window_rgb_top, 1);
+//    image_depth_pub = it.advertise(window_depth_top, 1);
+
+//    //初始化识别过程
+//    InitRecognition(window_rgb_top, window_depth_top);
+    cv::namedWindow("rgb_video");
+    cv::namedWindow("depth_video");
+    cv::startWindowThread();
+
+    if(depth_video_writer.open(depth_video_path, CV_FOURCC('M', 'J', 'P', 'G'),33,cv::Size(960, 540))==false)
+    {
+      ROS_ERROR("could not open depth video writer!!!");
+    }
+
+    if(rgb_video_writer.open(rgb_video_path, CV_FOURCC('M', 'J', 'P', 'G'),33,cv::Size(960,540))==false)
+    {
+      ROS_ERROR("could not open rgb video writer!!!");
+    }
+
+    //同步深度图和彩色图
+    message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo> sync(image_rgb_sub, image_depth_sub, cam_info_sub, 10);
+    sync.registerCallback(boost::bind(&RecognitionCallback, _1, _2, _3));
+
+    //ros主循环
+    ros::spin();
+    while(ros::ok());
+    depth_video_writer.release();
+    rgb_video_writer.release();
+
+    //释放资源
+//    ReleaseRecognition();
+    return 0;
+}
--- iai_kinect2/kinect2_recognition/src/kinect_recognition_node.cpp
+++ iai_kinect2/kinect2_recognition/src/kinect_recognition_node.cpp
@@ -3,8 +3,8 @@
 #include <string>
 
 //OpenCV头文件
-#include <opencv2/opencv.hpp>
-#include<opencv2/core.hpp>
+#include<opencv2/opencv.hpp>
+#include<opencv2/core/core.hpp>
 
 //ROS头文件
 #include <ros/ros.h>
@@ -24,11 +24,11 @@
 #include<pcl/visualization/cloud_viewer.h>
 
 //相机内参
-const double camera_factor = 1000;
-const double camera_cx = 325.5;
-const double camera_cy = 253.5;
-const double camera_fx = 518.0;
-const double camera_fy = 519.0;
+double camera_factor = 1000;
+double camera_cx = 325.5;
+double camera_cy = 253.5;
+double camera_fx = 518.0;
+double camera_fy = 519.0;
 
 //kinect相机话题
 std::string image_rgb_str =  "/kinect2/qhd/image_color_rect";
@@ -39,29 +39,25 @@ std::string cam_info_str = "/kinect2/qhd/camera_info";
 typedef pcl::PointXYZRGBA PointT;
 typedef pcl::PointCloud<PointT> PointCloud;
 
-void InitRecognition(std::string window_rgb_str, std::string window_depth_str, pcl::visualization::CloudViewer &viewer, pcl::PointCloud::Ptr cloud)
-{
-    //初始化显示
-    cv::namedWindow(window_rgb_str);
-    cv::namedWindow(window_depth_str);
-    cv::startWindowThread();
+//点云视窗
+pcl::visualization::CloudViewer viewer("pcd viewer");
+PointCloud::Ptr cloud(new PointCloud);
 
-    //初始化点云
-    cloud = new PointCloud;
+//图像显示
+std::string window_rgb_top = "/rgb_video";
+std::string window_depth_top = "/depth_video";
+image_transport::Publisher image_rgb_pub;
+image_transport::Publisher image_depth_pub;
 
-    //初始化视窗
-    viewer("pcd viewer");
-}
-
-void Recognition(cv::Rect2f& rect, cv::Mat image_rgb)
+void Recognition(cv::Rect& rect, cv::Mat image_rgb)
 {
     rect.x = 0;
     rect.y = 0;
-    rect.width = image_rgb.cols;
-    rect.height = image_rgb.rows;
+    rect.width = round(0.5*image_rgb.cols);
+    rect.height = round(0.5*image_rgb.rows);
 }
 
-void GetCloud(cv::Rect rect, cv::Mat image_rgb, cv::Mat image_depth, pcl::PointCloud::Ptr cloud)
+void GetCloud(cv::Rect rect, cv::Mat image_rgb, cv::Mat image_depth)
 {
     for(int i = rect.y;i<rect.y+rect.height;i++)
         for(int j = rect.x;j<rect.x+rect.width;j++)
@@ -95,41 +91,76 @@ void GetCloud(cv::Rect rect, cv::Mat image_rgb, cv::Mat image_depth, pcl::PointC
     cloud->is_dense = false;
 }
 
-void ShowCloud( pcl::visualization::CloudViewer &viewer, pcl::PointCloud::ConstPtr cloud)
+void ShowCloud( )
 {
     viewer.showCloud(cloud);
 }
 
-void RecognitionCallback(const sensor_msgs::ImageConstPtr& image_rgb, const sensor_msgs::ImageConstPtr & image_depth, const sensor_msgs::CameraInfoConstPtr & cam_info, pcl::visualization::CloudViewer &viewer, pcl::PointCloud::ConstPtr cloud)
+void RecognitionCallback(
+        const sensor_msgs::ImageConstPtr image_rgb,
+        const sensor_msgs::ImageConstPtr  image_depth,
+        const sensor_msgs::CameraInfoConstPtr  cam_info
+        )
 {
     //转换ROS图像消息到opencv图像
     cv::Mat mat_image_rgb = cv_bridge::toCvShare(image_rgb)->image;
     cv::Mat mat_image_depth = cv_bridge::toCvShare(image_depth)->image;
 
     //识别物体
-    cv::Rect2f Obj_Frame;
+    cv::Rect Obj_Frame;
     Recognition(Obj_Frame, mat_image_rgb);
 
     //相机内参提取
-    fx = cam_info->K[0] != 0?cam_info->K[0]:fx;
-    fy = cam_info->K[4] != 0?cam_info->K[4]:fy;
-    cx = cam_info->K[2] !=0?cam_info->K[2]:cx;
-    cy = cam_info->K[5] !=0?cam_info->K[5]:cy;
+    camera_fx = cam_info->K[0] != 0?cam_info->K[0]:camera_fx;
+    camera_fy = cam_info->K[4] != 0?cam_info->K[4]:camera_fy;
+    camera_cx = cam_info->K[2] !=0?cam_info->K[2]:camera_cx;
+    camera_cy = cam_info->K[5] !=0?cam_info->K[5]:camera_cy;
 
     //调整图像大小以便显示
-    cv::Mat image_rgb_show(640 ,480, CV_32FC1);
-    cv::Mat image_depth_show(640 ,480, CV_32FC1);
-    cv::resize(mat_image_rgb, image_rgb_show, image_rgb_show.size(), 0, 0, interpolation );
-    cv::resize(mat_image_depth, image_depth_show, image_depth_show.size(), 0, 0, interpolation);
+    cv::Mat image_rgb_show;
+    cv::Mat image_depth_show;
+    try
+    {
+        cv::resize(mat_image_rgb, image_rgb_show, cv::Size(480,640) );
+    }
+    catch (cv::Exception& e)
+    {
+        ROS_ERROR("Rgb image resizing failed");
+        image_rgb_show = mat_image_rgb.clone();
+    }
+    try
+    {
+        cv::resize(mat_image_depth, image_depth_show, cv::Size(480,640) );
+    }
+    catch (cv::Exception& e)
+    {
+        ROS_ERROR("Depth image resizing failed");
+        image_depth_show = mat_image_depth.clone();
+    }
+//     image_depth_show = mat_image_depth.clone();
+    //标注识别框
     float width_scale = image_rgb_show.cols/float(mat_image_rgb.cols), height_scale = image_rgb_show.rows/float(mat_image_rgb.rows);
-    cv::Rect2f Obj_Frame_Show(Obj_Frame.x*width_scale, Obj_Frame.y*height_scale, Obj_Frame.width*width_scale, Obj_Frame.height*height_scale);
+    cv::Rect Obj_Frame_Show(Obj_Frame.x*width_scale, Obj_Frame.y*height_scale, Obj_Frame.width*width_scale, Obj_Frame.height*height_scale);
     cv::rectangle(image_rgb_show, cv::Rect(round(Obj_Frame_Show.x), round(Obj_Frame_Show.y), round(Obj_Frame_Show.width), round(Obj_Frame_Show.height)), cv::Scalar(255,0,0));
+    cv::rectangle(image_depth_show, cv::Rect(round(Obj_Frame_Show.x), round(Obj_Frame_Show.y), round(Obj_Frame_Show.width), round(Obj_Frame_Show.height)), cv::Scalar(255));
+
+    ROS_INFO("the scale of the width is %f", width_scale);
+    ROS_INFO("the scale of the length is %f", height_scale);
+
+    //转换图像到ros消息
+    sensor_msgs::ImagePtr rgb_image_msg = cv_bridge::CvImage(std_msgs::Header(), image_rgb->encoding , image_rgb_show).toImageMsg();
+    sensor_msgs::ImagePtr depth_image_msg = cv_bridge::CvImage(std_msgs::Header(), image_depth->encoding , image_depth_show).toImageMsg();
+
+    //广播图像消息
+    image_rgb_pub.publish(rgb_image_msg);
+    image_depth_pub.publish(depth_image_msg);
+
 
     //显示彩色图和深度图
     try
     {
-        cv::imshow(window_rgb_str, image_rgb_show);
-        //cv::imshow("colored video", cv_bridge::toCvShare(image_color, "bgr8")->image);
+        //cv::imshow(window_rgb_top, cv_bridge::toCvShare(image_rgb)->image);
+        cv::imshow("rgb_video", image_rgb_show);
     }
     catch (cv_bridge::Exception& e)
     {
@@ -137,7 +168,7 @@ void RecognitionCallback(const sensor_msgs::ImageConstPtr& image_rgb, const sens
     }
     try
     {
-        cv::imshow(window_depth_str, image_depth_show);
+        cv::imshow("depth_video", image_depth_show);
     }
     catch(cv_bridge::Exception& e)
     {
@@ -146,49 +177,47 @@ void RecognitionCallback(const sensor_msgs::ImageConstPtr& image_rgb, const sens
 
     //提取对应点云
     cloud->points.clear();
-    GetCloud(cv::Rect(round(Obj_Frame.x), round(Obj_Frame.y), round(Obj_Frame.width), round(Obj_Frame.height)), mat_image_rgb, mat_image_depth, cloud );
+    GetCloud(cv::Rect(round(Obj_Frame.x), round(Obj_Frame.y), round(Obj_Frame.width), round(Obj_Frame.height)), mat_image_rgb, mat_image_depth );
 
     //显示点云
-    ShowCloud(viewer, cloud);
+    ShowCloud();
 }
 
-void ReleaseRecognition( pcl::PointCloud::Ptr cloud)
-{
-    delete cloud;
-    cv::destroyAllWindows();
-}
+//void ReleaseRecognition( )
+//{
+////    cv::destroyAllWindows();
+//}
 
 int main(int argc, char ** argv)
 {
     //启动ROS节点并获取句柄
-    ros::init(argc, argv, "kinect_recognition");
+    ros::init(argc, argv, "kinect2_recognition");
     ros::NodeHandle nh;
 
     //订阅话题
-    message_filters::Subscriber<sensor_msgs::Image> image_color_sub(nh, image_rgb_str, 1);
+    message_filters::Subscriber<sensor_msgs::Image> image_rgb_sub(nh, image_rgb_str, 1);
     message_filters::Subscriber<sensor_msgs::Image>image_depth_sub(nh, image_depth_str, 1);
     message_filters::Subscriber<sensor_msgs::CameraInfo>cam_info_sub(nh,  cam_info_str, 1);
 
-    //图像显示框
-    std::string window_rgb_str = "rgb video";
-    std::string window_depth_str = "depth video";
+    //发布话题
+    image_transport::ImageTransport it(nh);
+    image_rgb_pub = it.advertise(window_rgb_top, 1);
+    image_depth_pub = it.advertise(window_depth_top, 1);
 
-    //点云视窗
-    pcl::visualization::CloudViewer viewer();
-    PointCloud::Ptr cloud;
-
-    //初始化识别过程
-    InitRecognition(window_rgb_str, window_depth_str, viewer, cloud);
-
+//    //初始化识别过程
+//    InitRecognition(window_rgb_top, window_depth_top);
+    cv::namedWindow("rgb_video");
+    cv::namedWindow("depth_video");
+    cv::startWindowThread();
     //同步深度图和彩色图
-    message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image> sync(image_color_sub, image_depth_sub, cam_info_sub,   viewer,  cloud, 10);
-    sync.registerCallback(boost::bind(&RecognitionCallback, _1, _2, _3, _4, _5));
+    message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo> sync(image_rgb_sub, image_depth_sub, cam_info_sub, 10);
+    sync.registerCallback(boost::bind(&RecognitionCallback, _1, _2, _3));
 
     //ros主循环
     ros::spin();    
     while(ros::ok());
 
     //释放资源
-    ReleaseRecognition(cloud);
+//    ReleaseRecognition();
     return 0;
 }
